4c8187d4e2fae9a711f5074acaf67c5a691fe815
gullerya <gullerya@gmail.com>
2020-02-05 22:04:19 +0200

    updated dependencies, added funding option

--- tests/test-apis.js
+++ tests/test-apis.js
@@ -1,7 +1,7 @@
-﻿import { createSuite } from '../../node_modules/just-test/dist/just-test.min.js'
+﻿import { getSuite } from '../../node_modules/just-test/dist/just-test.min.js'
 import { tooltip, POSITIONS } from '../../dist/tooltip.js';
 
-const suite = createSuite({ name: 'Testing tooltip APIs' });
+const suite = getSuite({ name: 'Testing tooltip APIs' });
 
 suite.runTest({ name: 'test A' }, async test => {
 	const
